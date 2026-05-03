f2549eb4a44c2a511e15c27c9f45a0a26e742a55
luco <89469304+luwucoso@users.noreply.github.com>
2026-05-03 17:16:51 -0300

    simple path

--- code/player.gd
+++ code/player.gd
@@ -1,34 +1,84 @@
 class_name PlayerController extends Node
 
 @export var floor_layer: TileMapLayer
+@export var lines_layer: TileMapLayer
 @export var sprite: Sprite2D
+
+@export var move_speed: float = 40.0 
 
 var player_position: Vector2i = Vector2i.ZERO
 var target_position: Vector2
 
-@export var move_speed: float = 30.0 
+var path_history: Array[Vector2i] = []
+
+var undo_redo := UndoRedo.new()
+
 
 func _ready() -> void:
 	target_position = floor_layer.map_to_local(player_position)
 	sprite.position = target_position
+	
+	path_history.push_back(player_position)
+
 
 func _physics_process(delta: float) -> void:
+	if sprite.position.distance_to(target_position) > 1.0:
+		sprite.position = sprite.position.lerp(target_position, move_speed * delta)
+		return
+
 	var moved := false
+	var new_pos := player_position
 
 	if Input.is_action_just_pressed("move_right"):
-		player_position.x += 1
+		new_pos.x += 1
 		moved = true
 	elif Input.is_action_just_pressed("move_left"):
-		player_position.x -= 1
+		new_pos.x -= 1
 		moved = true
 	elif Input.is_action_just_pressed("move_down"):
-		player_position.y += 1
+		new_pos.y += 1
 		moved = true
 	elif Input.is_action_just_pressed("move_up"):
-		player_position.y -= 1
+		new_pos.y -= 1
 		moved = true
 
 	if moved:
-		target_position = floor_layer.map_to_local(player_position)
+		var old_pos := player_position
+
+		undo_redo.create_action("Move Player")
+
+		undo_redo.add_do_method(_apply_move.bind(new_pos))
+		undo_redo.add_do_method(_push_path.bind(new_pos))
+
+		undo_redo.add_undo_method(_apply_move.bind(old_pos))
+		undo_redo.add_undo_method(_pop_path)
+
+		undo_redo.commit_action()
+
+	if Input.is_action_just_pressed("undo"):
+		undo_redo.undo()
+
+	if Input.is_action_just_pressed("redo"):
+		undo_redo.redo()
 
 	sprite.position = sprite.position.lerp(target_position, move_speed * delta)
+
+
+func _apply_move(pos: Vector2i) -> void:
+	player_position = pos
+	target_position = floor_layer.map_to_local(player_position)
+	_update_path_visual()
+
+
+func _push_path(pos: Vector2i) -> void:
+	path_history.push_back(pos)
+	_update_path_visual()
+
+func _pop_path() -> void:
+	if path_history.size() > 1:
+		path_history.pop_back()
+	_update_path_visual()
+
+func _update_path_visual() -> void:
+	lines_layer.clear()
+	lines_layer.set_cells_terrain_path(path_history, 0, 0)
